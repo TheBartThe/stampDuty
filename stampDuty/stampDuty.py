689a9b1fbd6afe6e44a5bc8c4d0f723c1220c816
TheBartThe <vieran96@gmail.com>
2021-09-21 23:26:46 +0100

    added initial calculations of stamp duty

--- stampDuty/stampDuty.py
+++ stampDuty/stampDuty.py
@@ -10,10 +10,16 @@ def main():
 		except ValueError:
 			print("Input must be a number")
 	tax = firstTimeBuyer(price)
-	print(f'This will cost £{tax} in stamp duty')
+	if tax = -1:
+		print('Price must be positive')
+	else:
+		print(f'This will cost £{tax} in stamp duty')
 
 def firstTimeBuyer(price):
-	return 10
+	if (price < 0):
+		return -1
+	elif (price <= 250000):
+		return 0
 
 if __name__ == "__main__":
     main()
