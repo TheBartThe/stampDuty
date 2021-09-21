e81aadeb13892f65ca6df9dd19377f8a8d6a3480
TheBartThe <vieran96@gmail.com>
2021-09-21 23:30:51 +0100

    added unit tests for initial stamp duty calculations

--- tests/stampDuty_test.py
+++ tests/stampDuty_test.py
@@ -7,5 +7,20 @@ def testInput(monkeypatch):
 	assert stampDuty.main() == 10
 '''
 
-def testFTB():
-	assert stampDuty.firstTimeBuyer(20000) == 10
+def testFTBNeg1():
+	assert stampDuty.firstTimeBuyer(-100000) == -1
+
+def testFTBNeg2():
+	assert stampDuty.firstTimeBuyer(-1) == -1
+
+def testFTBFree1():
+	assert stampDuty.firstTimeBuyer(0) == 0
+
+def testFTBFree2():
+	assert stampDuty.firstTimeBuyer(10000) == 0
+
+def testFTBFree3():
+	assert stampDuty.firstTimeBuyer(100000) == 0
+
+def testFTBFree4():
+	assert stampDuty.firstTimeBuyer(250000) == 0
