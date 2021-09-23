7ff8a03b83d2d271a72dd751d6da4a6aa4cc02e6
TheBartThe <vieran96@gmail.com>
2021-09-23 16:38:25 +0100

    added more tax bands

--- stampDuty/stampDuty.py
+++ stampDuty/stampDuty.py
@@ -16,10 +16,28 @@ def main():
 		print(f'This will cost £{tax} in stamp duty')
 
 def firstTimeBuyer(price):
-	if (price < 0):
-		return -1
-	elif (price <= 250000):
+	band1Rate = 0.05
+	band2Rate = 0.1
+	band3Rate = 0.12
+	band0Price = 250000
+	band1Price = 925000
+	band2Price = 1500000
+	if (price <= band0Price):
 		return 0
+	elif (price <= band1Price):
+		tax = band1Rate * (price - 250000)
+		return tax
+	elif (price <= band2Price):
+		band1 = price - 925000
+		band2 = 675000
+		tax = (0.1 * band1) + (0.05 * band2)
+		return tax
+	else:
+		band1 = price - 1500000
+		band2 = 575000
+		band3 = 675000
+		tax = (0.12 * band1) + (0.1 * band2) + (0.05 * band3)
+		return tax
 
 if __name__ == "__main__":
     main()
--- tests/stampDuty_test.py
+++ tests/stampDuty_test.py
@@ -8,19 +8,36 @@ def testInput(monkeypatch):
 '''
 
 def testFTBNeg1():
-	assert stampDuty.firstTimeBuyer(-100000) == -1
+	assert stampDuty.firstTimeBuyer(-100000) == 0
 
 def testFTBNeg2():
-	assert stampDuty.firstTimeBuyer(-1) == -1
+	assert stampDuty.firstTimeBuyer(-1) == 0
 
 def testFTBFree1():
 	assert stampDuty.firstTimeBuyer(0) == 0
 
+	'''
 def testFTBFree2():
 	assert stampDuty.firstTimeBuyer(10000) == 0
 
 def testFTBFree3():
 	assert stampDuty.firstTimeBuyer(100000) == 0
+'''
 
 def testFTBFree4():
 	assert stampDuty.firstTimeBuyer(250000) == 0
+	
+def testFTBLow1():
+	assert stampDuty.firstTimeBuyer(300000) == 2500
+
+def testFTBLow2():
+	assert stampDuty.firstTimeBuyer(925000) == 33750
+
+def testFTBMid1():
+	assert stampDuty.firstTimeBuyer(950000) == 36250
+
+def testFTBMid2():
+	assert stampDuty.firstTimeBuyer(1500000) == 91250
+
+def testFTBHigh1():
+	assert stampDuty.firstTimeBuyer(1600000) == 103250
